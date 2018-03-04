9c4e853ad3312e472b019304c2cb440e9645e343
Jessie Leung <jleu1656@gmail.com>
2018-03-04 20:39:11 +1100

    Set up project

---
+++ app/src/main/java/au/com/mealplanner/mealplanner/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package au.com.mealplanner.mealplanner.main
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import au.com.mealplanner.mealplanner.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+    }
+}
